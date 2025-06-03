a50ddc84a40533988475559e6a4cc8a84560c0e6
Roberto Dev <robertofordevelopment@gmail.com>
2025-06-03 08:11:01 +0200

    added tailwind css

---
+++ src/app/gifs/pages/trending/trending.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending',
+  imports: [],
+  templateUrl: './trending.html',
+  standalone: true,
+  styleUrl: './trending.css'
+})
+export default class Trending {
+
+}
